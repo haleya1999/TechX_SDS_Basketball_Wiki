730421b2179b693ff4e7f4bd5a86ab7c8d4fd80a
Khloe Wright <kpwright@techexchange.in>
2023-03-06 22:21:39 +0000

    Getting all page names from bucket

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -6,13 +6,17 @@ storage_client = storage.Client()
 class Backend:
 
     def __init__(self):
-        pass
+        self.page_names = []
         
     def get_wiki_page(self, name):
         pass
 
     def get_all_page_names(self):
-        pass
+        bucket_name = "wiki-contents"
+        all_names = storage_client.list_blobs(bucket_name)
+        for page_name in all_names:
+            self.page_names.append(page_name)
+        return self.page_names
 
     def upload(self):
         pass
