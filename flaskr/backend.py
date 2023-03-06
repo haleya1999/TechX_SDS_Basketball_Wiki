8f0b0ef8795147cce4230822301c6ce1abac2394
Haley Arzu <haley_arzu@techexchange.in>
2023-03-06 06:27:56 +0000

    set user and content buckets and finished get_image

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -2,6 +2,8 @@
 from google.cloud import storage
 
 storage_client = storage.Client()
+content_bucket = storage_client.bucket('wiki-contents')
+user_bucket = storage_client.bucket('users-passwds')
 
 class Backend:
 
@@ -23,6 +25,8 @@ class Backend:
     def sign_in(self):
         pass
 
-    def get_image(self):
-        pass
-
+    def get_image(self, img_name):
+        # for a given image name in GCS Bucket, make image public and return public url
+        blob = content_bucket.blob(img_name)
+        blob.make_public()
+        return blob.public_url
