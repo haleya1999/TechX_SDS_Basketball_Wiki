965172e0b9243fe4dc2445f5300948e0631db663
Khloe Wright <kpwright@techexchange.in>
2023-04-19 19:05:15 +0000

    Actually updating metadata file

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -246,10 +246,13 @@ class Backend:
         '''
         print(source_name)
         print("gets to metadata")
-        self.metadata_page = self.content_bucket.blob(f"metadata/{source_name}")
-        modified_data = None
+        self.metadata_page = self.content_bucket.get_blob(f"metadata/{source_name}")
+        modified_data = ""
         print(self.metadata_page)
-        with self.metadata_page.open("r") as metadata_page:
+        f = open(f"{source_name}", "w") 
+        self.metadata_page.download_to_filename(f"{source_name}")
+        f.close()
+        with open(f"{source_name}", "r") as metadata_page:
             data = metadata_page.readlines()
             print(data)
             visits = data[2]
@@ -257,12 +260,21 @@ class Backend:
             amt_visits += 1
             print(amt_visits)
             data[2] = f"Number of Vists: {amt_visits}\n"
-            modified_data = data
-            print(modified_data)
-        with self.metadata_page.open("w") as metadata_page:
+            modified_data = modified_data.join(data)
+            print(modified_data)            
+        # with self.metadata_page.open("r") as metadata_page:
+        #     data = metadata_page.readlines()
+        #     print(data)
+        #     visits = data[2]
+        #     amt_visits = int(visits[-2])
+        #     amt_visits += 1
+        #     print(amt_visits)
+        #     data[2] = f"Number of Vists: {amt_visits}\n"
+        #     modified_data = modified_data.join(data)
+        #     print(modified_data)
+        with open(f"{source_name}", "w") as metadata_page:
             metadata_page.writelines(modified_data)
-        blob = self.metadata_page
-        blob.upload_from_filename(source_name)
+        self.metadata_page.upload_from_filename(f"{source_name}")
 
         
     def sign_up(self, username, password):
